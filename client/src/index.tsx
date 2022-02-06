1a1d8889dd72a252c57abb5acbc476833c514f17
leocrossman <lkcrossman@gmail.com>
2022-02-05 19:59:17 -0500

    reorganized project file structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
-import { store } from './app/store';
+import App from './pages/App/App';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
--- client/src/pages/App/App.tsx
+++ client/src/pages/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import logo from '../../logo.svg';
+import { Counter } from '../../Components/counter/Counter';
 import './App.scss';
 
 function App() {
