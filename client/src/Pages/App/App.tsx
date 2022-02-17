ed129aff1728e5ef87554e9f4cfcd83b35e1eba2
leocrossman <lkcrossman@gmail.com>
2022-02-16 23:13:43 -0500

    added home component

--- client/src/Pages/App/App.tsx
+++ client/src/Pages/App/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useBattery } from 'react-use';
 import './App.scss';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../Home/Home';
 function App() {
   return (
     <div className='App-container'>
-      <div>HELLO</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
